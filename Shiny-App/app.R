418a636e66c271f843dd9b0476598fc2ae48c8f8
pablogarriga <31629971+pablogarriga@users.noreply.github.com>
2018-12-15 14:09:36 -0500

    work on app

--- Shiny-App/app.R
+++ Shiny-App/app.R
@@ -1,10 +1,30 @@
-#This is where we rite the code for our Shiny App
+#SHINY APP
 
+#**************************************************************
+# APP SETUP
+#**************************************************************
+
+# Load packages
 library(shiny)
 
+# load basefile
+# basefile <- readRDS("data/basefile.Rds")
+
+#load codebooks
+# codebook <- read_excel("data/codebook.xlsx")
+# quickcode <- read_excel("data/modal_codebook.xlsx")
+
+
+
+#**************************************************************
+# UI
+#**************************************************************
+
+
+# Begin fluidPage
 ui = fluidPage(
   sliderInput(inputId = "num",
-              label = "Coose a Number",
+              label = "Choose a Number",
               value = 1, min = 1, max = 100),
   plotOutput("hist")
     
