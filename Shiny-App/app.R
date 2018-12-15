ac01f6c418cdf2931d20d0e79e59072db877ee4f
pablogarriga <31629971+pablogarriga@users.noreply.github.com>
2018-12-15 17:16:28 -0500

    progress on shiny app

--- Shiny-App/app.R
+++ Shiny-App/app.R
@@ -9,7 +9,7 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 
-
+# load(paste0("./bulkdata/data/contributoins_","CA"))
 #**************************************************************
 # UI
 #**************************************************************
@@ -24,23 +24,21 @@ ui = fluidPage(
     
     sidebarPanel("our inputs will go here",
                  
-      sliderInput(inputId = "num",
-                  label = "Choose a Number",
-                  value = 1, min = 1, max = 100
-      ),
-      
       selectInput(
         inputId = "state",
         label = "Choose a State",
-        # c("AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY")
         c("AZ","CA","CT")
-      )
+      ),
+      uiOutput(outputId = "candlist_dem"),
+      uiOutput(outputId = "candlist_rep")
+      
+      
     ),
-    
+  
     
     mainPanel("the results will go here",
-      plotOutput("hist"),
-      tableOutput("state")
+      plotOutput("avg_trends")
+
     )
   )
 )
@@ -52,15 +50,81 @@ ui = fluidPage(
 # Begin server
 server = function(input, output) {
   
-  output$hist = renderPlot({
-    hist(rnorm(input$num))
+  output$candlist_dem = renderUI({
+    load(paste0("./bulkdata/data/contributoins_",input$state))
+    candidates = df %>% filter(party =="DEM")
+    candidates = unique(candidates$candidate)
+    radioButtons("candidates_dem", "Pick one Democratic candidate", candidates)
   })
   
-  output$state = renderTable({
+  output$candlist_rep = renderUI({
     load(paste0("./bulkdata/data/contributoins_",input$state))
-    filtered = df %>% filter(contribution_receipt_amount >= 25000)
-    filtered
+    candidates = df %>% filter(party =="REP")
+    candidates = unique(candidates$candidate)
+    radioButtons("candidates_rep", "Pick one Republican candidate", candidates)
   })
+ 
+  input$cand_dem = renderText({
+    output$candlist_dem
+  })
+  
+  input$cand_rep = renderText({
+    output$candlist_rep
+  })  
+
+  output$avg_trends = renderPlot({
+    load(paste0("./bulkdata/data/contributoins_",input$state))
+    
+    data = df %>% filter(candidate == input$cand_dem | input$cand_rep)
+  
+    plot_avg_donation <- function(df) {
+      
+      # Initialize graph attributes 
+      graph_theme = theme_bw(base_size = 12) +
+        theme(panel.grid.major = element_line(size = .1, color = "grey"), # Increase size of gridlines 
+              axis.line = element_line(size = .7, color = "black"), # Increase size of axis lines 
+              axis.text.x = element_text(angle = 90, hjust = 1), #Rotate text
+              text = element_text(size = 12)) # Increase the font size
+      group_colors = c("#377EB8", "#E41A1C") # blue, red
+      
+      # Get dates
+      dates <- df %>% select(contribution_receipt_date) %>%  mutate(date = as.Date(contribution_receipt_date)) %>%
+        summarise(min = min(date), max = max(date))
+      title <- paste("From", dates$min, "To", dates$max)
+      
+      # Extract party from the committee list
+      df$party <- as.character(lapply(df$committee, `[[`, "party"))
+      
+      # Donation data cleanup: clean
+      data_clean = df %>% 
+        mutate(amount = contribution_receipt_amount, date = as.Date(contribution_receipt_date)) %>%
+        select(amount, date, party) %>%
+        mutate(date = as.Date(date))
+      
+      # Donation data cleanup: average daily
+      data_average_daily <- data_clean %>% 
+        group_by(party, date) %>%
+        summarise(Mean = mean(amount), SD = sd(amount), N = n(), SE = SD / sqrt(N), na.rm = TRUE)
+      
+      plot_average_daily <- ggplot(data_average_daily,
+                                   aes(x = date, y = Mean, group = party, color = party)) + 
+        geom_point() + 
+        geom_smooth(method = "loess") + # loess
+        xlab(label = "Date") +
+        scale_y_continuous(name = "Average Donation per Contributor") +    
+        ggtitle(paste("Senate Donations", title, "\nDaily")) + 
+        scale_color_manual(values = group_colors) +
+        graph_theme 
+      
+      return(plot_average_daily)
+    }
+    
+    plot_avg_donation(data)
+    
+
+  })
+  
+   
 }
 
 shinyApp(ui = ui, server = server)
