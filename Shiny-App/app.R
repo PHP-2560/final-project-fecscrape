eb97d043d553cc93f31693f5ee8fd8c0f2f569a9
Gabriele <gabriele_borg@brown.edu>
2018-12-16 17:36:51 -0500

    added top_cities and top_occ graphs

--- Shiny-App/app.R
+++ Shiny-App/app.R
@@ -8,6 +8,8 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(scales)
+library(shinythemes)
 
 # load(paste0("./bulkdata/data/contributoins_","CA"))
 #**************************************************************
@@ -16,6 +18,7 @@ library(dplyr)
 
 # Begin fluidPage
 ui = fluidPage(
+  theme = shinytheme("cerulean"),
 
   titlePanel("FEC individual donations"),
   
@@ -44,7 +47,21 @@ ui = fluidPage(
     
     mainPanel(
       plotOutput("avg_trends"),
-      plotOutput("cum_trends")
+      plotOutput("cum_trends"), 
+    
+      plotOutput("top_cities"),
+      sliderInput(inputId = "num_cities", 
+                  label = "Select the number of top cities to display",
+                  min = 1, 
+                  max = 10, 
+                  value = 2), 
+      
+      plotOutput("top_occ"),
+      sliderInput(inputId = "num_occ", 
+                  label = "Select the number of top occupations to display",
+                  min = 1, 
+                  max = 8, 
+                  value = 2)
 
     )
   )
@@ -106,7 +123,7 @@ server = function(input, output) {
 
       plot_average_daily <- ggplot(data_average_daily,
                                    aes(x = date, y = Mean, group = party, color = party)) +
-        geom_point() +
+        # geom_point() +
         geom_smooth(method = "loess") + # loess
         xlab(label = "Date") +
         scale_y_continuous(name = "Average Donation per Contributor") +
@@ -166,8 +183,143 @@ server = function(input, output) {
     plot_cum_donation(data_trend())
   })
   
+  output$top_cities <- renderPlot({
+    plot_top_cities <- function(n, df) {
+      #aggregate by city
+      citydf <- df %>%
+        mutate(city_state = paste0(contributor_city, ", ", contributor_state)) %>%
+        group_by(candidate, city_state) %>%
+        summarize(party = first(party) , count = n(), total = sum(contribution_receipt_amount, na.rm = T), average = mean(contribution_receipt_amount, na.rm = T), sd = sd(contribution_receipt_amount, na.rm = T), min = min(contribution_receipt_amount, na.rm = T), max = max(contribution_receipt_amount, na.rm = T)) %>%
+        arrange(desc(total)) %>%
+        top_n(n, total)
+      
+      citydf
+      
+      ## GRAPH
+      # set theme
+      graph_theme = theme_bw(base_size = 12) +
+        theme(panel.grid.major = element_line(size = .1, color = "grey"), # Increase size of gridlines
+              axis.line = element_line(size = .7, color = "black"), # Increase size of axis lines
+              text = element_text(size = 14)) # Increase the font size
+      
+      if (citydf$party[1]=="DEM") { #this if statement assign the right color to each candidate: red for Republicans and Blue for Democrats
+        group_colors <- c("#377EB8", "#E41A1C") # set colors
+      } else {
+        group_colors <- c("#E41A1C", "#377EB8") # set colors
+      }
+      
+      #prepare labels
+      brk<-citydf$total %>%
+        range() %>%
+        max()
+      brk<-c(brk/2,brk)
+      brk<-  round(brk/10000,0)*10000
+      brk<-c(-brk,0,brk)
+      brk<-sort(brk)
+      brk
+      lab<-abs(brk)
+      
+      #plot
+      plot<- citydf %>%
+        mutate(total = ifelse(party=="REP", -total, total )) %>%
+        arrange(desc(total), party) %>%
+        ggplot(aes(x = reorder(city_state, -total), y = total, fill = candidate, group = candidate)) +
+        geom_bar(stat = "identity") +
+        coord_flip() +
+        xlab("Top Donations Cities") +
+        scale_y_continuous(breaks = brk, labels=lab, name = "Total Donations")  +
+        scale_fill_manual(values = group_colors) +
+        graph_theme
+      
+      
+      output<-list(citydf, plot)
+      
+      return(output)
+    }
+    
+    plot_top_cities(input$num_cities,data_trend())
+  })
   
-  
+  output$top_occ <- renderPlot({
+    plot_occupations <- function(n, df) {
+      
+      if (n >= 8 ) n = 8
+ 
+      #compute total donations by party
+      totdon<-df %>%
+        group_by(candidate) %>%
+        summarize(count1 = n(), tot = sum(contribution_receipt_amount))
+      #totdon
+      
+      #compute total donation for top 5 occupations for each party
+      tottop<-df %>%
+        mutate(city_state = paste0(contributor_city, ", ", contributor_state)) %>%
+        group_by(candidate, contributor_occupation) %>%
+        summarize(count = n(), total_donations = sum(contribution_receipt_amount)) %>%
+        arrange(desc(total_donations)) %>%
+        top_n(n, total_donations) %>%
+        group_by(candidate) %>%
+        summarize(count2 = sum(count), tottop = sum(total_donations))
+      #tottop
+      
+      # generate residual donations category (not top 5 occupation) to create percentage stacked bar
+      others<-totdon %>%
+        left_join(tottop) %>%
+        mutate(count = count1 - count2, total = tot - tottop) %>%
+        mutate(contributor_occupation = "OTHERS") %>%
+        select("candidate", "contributor_occupation", "count", "total")
+      #others
+      
+      #prepare summary stats
+      topocc_data<-df %>%
+        group_by(candidate, contributor_occupation) %>%
+        summarize(party = first(party) , count = n(), total = sum(contribution_receipt_amount, na.rm = T), average = mean(contribution_receipt_amount, na.rm = T), sd = sd(contribution_receipt_amount, na.rm = T), min = min(contribution_receipt_amount, na.rm = T), max = max(contribution_receipt_amount, na.rm = T)) %>%
+        arrange(desc(total)) %>%
+        top_n(n, total) %>%
+        arrange(candidate, desc(total))
+      topocc_data$contributor_occupation[is.na(topocc_data$contributor_occupation)]<-"NOT AVAILABLE"
+      
+      topocc_data
+      
+      ## GRAPH
+      #graph theme settings
+      graph_theme = theme_bw(base_size = 12) +
+        theme(panel.grid.major = element_line(size = .1, color = "grey"), # Increase size of gridlines
+              axis.line = element_line(size = .7, color = "black"), # Increase size of axis lines
+              text = element_text(size = 12), legend.text=element_text(size=rel(0.7))) # Increase the font size
+      
+      
+      blues<-RColorBrewer::brewer.pal(n+1, "Blues")
+      reds<-RColorBrewer::brewer.pal(n+1, "Reds")
+      
+      if (topocc_data$party[1]=="DEM") {
+        cols<-c(blues, reds)
+      } else {
+        cols<-c(reds, blues)
+      }
+      
+      #add others category to data
+      topocc_data<-topocc_data %>%
+        bind_rows(others)
+      
+      topocc_data$contributor_occupation<-factor(topocc_data$contributor_occupation, levels = unique(topocc_data$contributor_occupation)[order(topocc_data$total, decreasing = F)])
+      
+      #prepare data for barplot
+      plot_final <- topocc_data %>%
+        arrange(candidate, desc(total)) %>%
+        ggplot(aes(x = candidate, y = total, fill = interaction(contributor_occupation, candidate))) +
+        geom_bar(stat= "identity", position = "fill") +
+        scale_y_continuous(labels = percent_format()) +
+        # scale_fill_hue(h = c(45, 365)) +
+        scale_fill_manual(values = cols, name = "Top Occupations")+
+        ylab("Donation Shares") +
+        labs(caption = "The OTHERS category groups all the residual occupations not in the top-n positions") +
+        graph_theme
+      
+      return(plot_final)
+    }
+    plot_occupations(input$num_occ,data_trend())
+  })
   
      
 }
